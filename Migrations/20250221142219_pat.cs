d35c3ae7fd141cb43dd0ce8e77e79d25551cd56d
Max_pain <yaseno933@gmail.com>
2025-02-21 16:25:12 +0200

    patient fix

---
+++ Migrations/20250221142219_pat.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediMed.Migrations
+{
+    /// <inheritdoc />
+    public partial class pat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Approved",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PersonalPicture",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "Nurses",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Nurses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PersonalPicture",
+                table: "Nurses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Approved",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "PersonalPicture",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Nurses");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Nurses");
+
+            migrationBuilder.DropColumn(
+                name: "PersonalPicture",
+                table: "Nurses");
+        }
+    }
+}
